61e7acfa11b760e27cafae7dccac0d3ef9b806cc
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-09-04 18:06:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,25 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get '/' do 
+      erb :new
+    end
+    
+    post '/pirates' do 
+      
+    @pirate = Pirate.new(params[:pirate])
+ 
+    params[:pirate][:ship].each do |details|
+    Ship.new(details)
+    end
+ 
+    @ship = Ship.all
+ 
+    erb :show
+    end 
+    
+    get '/new' do 
+      erb :new
+    end 
   end
 end
